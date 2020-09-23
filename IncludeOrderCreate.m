4125dd50a47e6e14898dbcf6c68d5c4da9ff9edf
lqchel <liangqianchen@hotmail.com>
2020-09-23 14:22:19 +1000

    updates

--- IncludeOrderCreate.m
+++ IncludeOrderCreate.m
@@ -6,7 +6,9 @@ for subject = 1:1
         fid = fopen(filename,'w');
         fprintf(fid,'<include>\n');
 %         fprintf(fid,'/ file = "ConcentrationResponse.iqx"\n');
-        fprintf(fid,'/ file = "Calibration.iqx"\n');
+        fprintf(fid,'/ file = "calibration_demo.iqx"\n');
+        fprintf(fid,'/ file = "SchizotypySurvey.iqx"\n');
+        fprintf(fid, '/ file = "participant_demographics.iqx"\n');
         fprintf(fid,'/ file = "PictureAndText_V2.iqx"\n');
         fprintf(fid,'/ file = "Instruction.iqx"\n');
         fprintf(fid,'/ file = "MainStructure_V2.iqx"\n');

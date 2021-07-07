29311f740a9a537756e9bb9f2b5a8207e70cd7fc
lqchel <liangqianchen@hotmail.com>
2021-07-07 11:07:54 +1000

    update for final data collection

--- GroupScriptCreate.m
+++ GroupScriptCreate.m
@@ -6,10 +6,17 @@ for batch = 1:4
         DesDir = sprintf('Batch_%d_Group_%d',batch,group);
         DST_PATH_t = ['WebVersion\',DesDir];%??????
         fid = fopen(filename,'w');
-            for subject = 1:15
+            for subject = 1:45
                 fprintf(fid,'<batch>\n');
-                fprintf(fid,'/subjects = (%d of 15)\n',subject);
-                fprintf(fid,'/file = "Script_B%d_G%d_S%d.iqx"\n',batch,group,subject);
+                fprintf(fid,'/subjects = (%d of 45)\n',subject);
+                if subject >= 1 && subject <= 15
+                    script_subject_code = subject;
+                elseif subject > 15 && subject <= 30
+                    script_subject_code = subject - 15;
+                elseif subject > 30
+                    script_subject_code = subject - 30;
+                end
+                fprintf(fid,'/file = "Script_B%d_G%d_S%d.iqx"\n',batch,group,script_subject_code);
                 fprintf(fid,'</batch>\n\n');
             end
     end
